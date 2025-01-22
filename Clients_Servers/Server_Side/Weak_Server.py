65c1569fa10b0844fea3cbcc82377bb4247f55b8
cossu555 <49615166+cossu555@users.noreply.github.com>
2025-01-22 10:37:45 +0100

    Update Weak_Server.py
    add simple code

--- Clients_Servers/Server_Side/Weak_Server.py
+++ Clients_Servers/Server_Side/Weak_Server.py
@@ -1,4 +1,4 @@
-# Import needed libraries
+"""# Import needed libraries
 import socket
 from threading import Thread
 
@@ -37,3 +37,17 @@ def handle_client(conn):
         print(f"Received: {data.decode('utf-8')}")
         # Send a response back to the client
         conn.sendall(b"Received")
+"""
+
+def start_server(host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+    print(f"server listening on {host}:{port}")
+
+    client_socket,client_address = server_socket.accept()
+
+    print(f"Connection Accepted from {client_address}")
+    client_socket.close()
+    server_socket.close()
