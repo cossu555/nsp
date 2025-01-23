c2a6f2a1e97a5bf744027bb81ef24ec2d4afb1a6
cossu555 <49615166+cossu555@users.noreply.github.com>
2025-01-23 22:34:39 +0100

    Update Weak_Server.py

--- VirtualM/Malevolent/Weak_Server.py
+++ VirtualM/Malevolent/Weak_Server.py
@@ -3,4 +3,4 @@ import Clients_Servers.Server_Side.Weak_Server as W_S
 host = input("Enter for server side: (suggestion: 0.0.0.0)")
 port = int(input("Enter port: (suggestion: 443)"))
 
-W_S.start_server(host, port)
+W_S._start_server(host, port)
